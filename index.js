dcfc7b6e58e0b0a22429903f581967d0b0ca9b2e
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-01-26 11:50:51 -0800

    updates

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict'
 
 module.exports = {
-  Command: require('./command'),
-  api: require('./mixins/api'),
-  app: require('./mixins/app'),
-  auth: require('./mixins/auth')
+  Command: require('./lib/command'),
+  convertLegacy: require('./lib/legacy'),
+  api: require('./lib/mixins/api'),
+  app: require('./lib/mixins/app'),
+  auth: require('./lib/mixins/auth')
 }
