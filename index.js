12a642aa41ba8d9d8396bced56103ca18a2e9060
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-16 14:59:21 -0800

    remove commandlist

--- index.js
+++ index.js
@@ -3,6 +3,5 @@
 module.exports = {
   Command: require('./lib/command'),
   convertLegacy: require('./lib/legacy'),
-  mixins: require('./lib/mixins'),
-  CommandList: require('./lib/command_list')
+  mixins: require('./lib/mixins')
 }
