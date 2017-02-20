a76c1d10d31b9238e89de17c810a1c2ebaee4628
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-20 15:36:08 -0800

    flowed color

--- src/color.js
+++ src/color.js
@@ -1,43 +1,46 @@
 // @flow
 /* globals
    Class
+   $Shape
  */
 
 import supports from 'supports-color'
-import Base from './base'
+import Base, {type Config} from './base'
 import chalk from 'chalk'
 
-type Color = typeof chalk
+const CustomColors = {
+  attachment: s => chalk.cyan(s),
+  addon: s => chalk.yellow(s),
+  configVar: s => chalk.green(s),
+  release: s => chalk.blue.bold(s),
+  cmd: s => chalk.cyan.bold(s),
+  app: s => process.platform !== 'win32' ? CustomColors.heroku(`⬢ ${s}`) : CustomColors.heroku(s),
+  heroku: s => {
+    let has256 = supports.has256 || (process.env.TERM || '').indexOf('256') !== -1
+    return has256 ? '\u001b[38;5;104m' + s + chalk.styles.reset.open : chalk.magenta(s)
+  }
+}
+
+type Color = $Shape<typeof chalk & typeof CustomColors>
 
 export default (Base: Class<Base>) => class extends Base {
-  get supportsColor (): typeof supports {
-    if (this.slack) return false
-    if (['false', '0'].indexOf((process.env.COLOR || '').toLowerCase()) !== -1) return false
+  constructor (config: Config) {
+    super(config)
+    if (!this.supportsColor) chalk.enabled = false
+  }
+
+  get supportsColor (): typeof supports | null {
+    if (this.slack) return
+    if (['false', '0'].indexOf((process.env.COLOR || '').toLowerCase()) !== -1) return
     return supports
   }
 
-  _color: Color
-
   get color (): Color {
     if (this._color) return this._color
-
-    this._color = chalk
-    this._color.enabled = !!this.supportsColor
-
-    this._color.attachment = s => this._color.cyan(s)
-    this._color.addon = s => this._color.yellow(s)
-    this._color.configVar = s => this._color.green(s)
-    this._color.release = s => this._color.blue.bold(s)
-    this._color.cmd = s => this._color.cyan.bold(s)
-
-    this._color.heroku = s => {
-      if (!this.supportsColor) return s
-      let has256 = this.supportsColor.has256 || (process.env.TERM || '').indexOf('256') !== -1
-      return has256 ? '\u001b[38;5;104m' + s + this._color.styles.modifiers.reset.open : this._color.magenta(s)
-    }
-
-    this._color.app = s => this.supportsColor && process.platform !== 'win32' ? this._color.heroku(`⬢ ${s}`) : this._color.heroku(s)
-
-    return (this._color: Color)
+    this._color = new Proxy({}, {get: (_, name) => {
+      if (CustomColors[name]) return CustomColors[name]
+      return chalk[name]
+    }})
+    return this._color
   }
 }
