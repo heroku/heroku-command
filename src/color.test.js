15a79b0e9b8fc88e96746ae460fa5e1d5b7447ec
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-20 15:36:08 -0800

    flowed color

--- src/color.test.js
+++ src/color.test.js
@@ -2,6 +2,7 @@
 
 /* globals
   test
+  expect
   */
 
 import Base from './base'
@@ -11,6 +12,5 @@ class ColorTester extends color(Base) {}
 
 test('shows if color is available', () => {
   const tester = new ColorTester({name: 'foo', version: '1.0'})
-  console.log(tester.color.ap('f'))
-  // console.dir(tester.color)
+  expect(tester.color.app('myapp')).toEqual('\u001b[38;5;104m⬢ myapp\u001b[0m')
 })
