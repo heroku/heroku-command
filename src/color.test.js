728e584018476e651147885bb059b8ea22401fd8
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-20 15:51:21 -0800

    fix tests when color is disabled

--- src/color.test.js
+++ src/color.test.js
@@ -7,10 +7,13 @@
 
 import Base from './base'
 import color from './color'
+import {DefaultConfig} from '../test/constants.js'
+import chalk from 'chalk'
 
 class ColorTester extends color(Base) {}
 
 test('shows if color is available', () => {
-  const tester = new ColorTester({name: 'foo', version: '1.0'})
+  const tester = new ColorTester(DefaultConfig)
+  chalk.enabled = true
   expect(tester.color.app('myapp')).toEqual('\u001b[38;5;104m⬢ myapp\u001b[0m')
 })
