8986256cd373f126da9262729fab1e26b26e57f9
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-22 20:39:00 -0800

    got varargs working

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@
 export {default} from './command'
 export {default as Base} from './base'
 export {default as Config, ConfigOptions} from './config'
+export {default as Topic} from './topic'
+export type {Flag} from './flag'
+export type {Arg} from './arg'
