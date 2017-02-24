391c7c8c1efd0b98ba7a39aec9500edae09a6277
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-24 12:40:29 -0800

    more test cases

--- src/output/action.test.js
+++ src/output/action.test.js
@@ -14,8 +14,15 @@ CustomColors.supports = {has256: true}
 class Output extends Base {}
 
 test('shows action', () => {
-  const cmd = new Output({mock: true})
-  cmd.action.start('doing a foo')
-  cmd.action.stop()
-  expect(cmd.stderr.output).toEqual('doing a foo... \ndoing a foo... done\n')
+  const out = new Output({mock: true})
+  out.action.start('doing a foo')
+  out.action.stop()
+  expect(out.stderr.output).toEqual('doing a foo... \ndoing a foo... done\n')
+})
+
+test('implicit done', async () => {
+  const out = new Output({mock: true})
+  out.action.start('doing a foo')
+  await out.done()
+  expect(out.stderr.output).toEqual('doing a foo... \ndoing a foo... done\n')
 })
