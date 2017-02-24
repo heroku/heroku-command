47a8f525eabceba20d9a19a3cfd8aec6d9d70207
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-24 13:14:24 -0800

    added config tests

--- src/config.test.js
+++ src/config.test.js
@@ -7,10 +7,29 @@
 
 import Config from './config'
 import format from 'pretty-format'
+import os from 'os'
+import path from 'path'
+
+const config = new Config({argv: ['heroku', 'apps']})
 
 describe('inspect', () => {
   test('prints the object', () => {
-    let config = new Config({argv: ['heroku', 'apps']})
     expect(format(config)).toMatch(/^Object {/)
   })
+})
+
+test('props are set', () => {
+  expect(config.name).toEqual('cli-engine-command')
+  expect(config.version).toEqual('2.0.6')
+  expect(config.channel).toEqual('stable')
+  expect(config.argv).toEqual(['heroku', 'apps'])
+  expect(config.mock).toEqual(false)
+  expect(config.updateDisabled).toEqual(undefined)
+  expect(config.bin).toEqual('cli-engine-command')
+  expect(config.binPath).toEqual(undefined)
+  expect(config.root).toEqual(path.join(__dirname, '..'))
+  expect(config.defaultCommand).toEqual('help')
+  expect(config.s3).toEqual({})
+  expect(config._cli).toEqual({})
+  expect(config.windows).toEqual(os.platform === 'win32')
 })
