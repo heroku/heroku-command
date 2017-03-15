5f072dc0d3148a84c385c389d04dc70efc5af06e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-03-15 14:43:14 -0700

    added app git remote lookup

--- src/mixins/heroku.js
+++ src/mixins/heroku.js
@@ -2,10 +2,36 @@
 
 import http from '../http'
 import type Command from '../command'
+import url from 'url'
 
 type Options = {
   required?: boolean
 }
+
+const host = process.env.HEROKU_HOST || 'heroku.com'
+const apiHost = host.startsWith('http') ? host : `https://api.${host}`
+let gitHost = process.env.HEROKU_GIT_HOST
+if (!gitHost) {
+  if (host.startsWith('http')) {
+    const u = url.parse(host)
+    if (u.host) gitHost = u.host
+  }
+  if (!gitHost) gitHost = host
+}
+let httpGitHost = process.env.HEROKU_GIT_HOST
+if (!httpGitHost) {
+  if (host.startsWith('http')) {
+    const u = url.parse(host)
+    if (u.host) httpGitHost = u.host
+  }
+  httpGitHost = `git.${host}`
+}
+
+export const gitPrefixes = [
+  `git@${gitHost}:`,
+  `ssh://git@${gitHost}/`,
+  `https://${httpGitHost}/`
+]
 
 export default class Heroku extends http {
   options: Options
@@ -25,7 +51,7 @@ export default class Heroku extends http {
     if (!auth) {
       const Netrc = require('netrc-parser')
       const netrc = new Netrc()
-      auth = netrc.machines['api.heroku.com'].password
+      auth = netrc.machines[apiHost].password
     }
     if (!auth && this.options.required !== false) {
       throw new Error('Not logged in')
