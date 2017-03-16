35387966716ce4f35b6f5d34b8ce4c7786173546
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-15 23:34:53 -0700

    increase code coverage

--- src/mixins/heroku.test.js
+++ src/mixins/heroku.test.js
@@ -2,34 +2,51 @@
 
 import nock from 'nock'
 import Base from '../command'
-import Heroku from './heroku'
+import Config from '../config'
+import Heroku, {Vars} from './heroku'
 import Netrc from 'netrc-parser'
 
 jest.mock('netrc-parser')
-
-// flow$ignore
-Netrc.mockImplementation(() => {
-  return {machines: {'api.heroku.com': {password: 'mypass'}}}
-})
 
 class Command extends Base {
   heroku = new Heroku(this, {required: false})
 }
 
-let api
-beforeEach(() => {
-  api = nock('https://api.heroku.com')
-})
-afterEach(() => {
-  api.done()
+describe('vars', () => {
+  it('sets vars by default', () => {
+    const vars = new Vars({})
+    expect(vars.host).toEqual('heroku.com')
+    expect(vars.gitHost).toEqual('heroku.com')
+    expect(vars.httpGitHost).toEqual('git.heroku.com')
+  })
+
+  it('respects HEROKU_HOST', () => {
+    const vars = new Vars({HEROKU_HOST: 'https://customhost'})
+    expect(vars.host).toEqual('https://customhost')
+    expect(vars.gitHost).toEqual('customhost')
+    expect(vars.httpGitHost).toEqual('customhost')
+  })
 })
 
-test('makes an HTTP request', async () => {
-  api.get('/apps')
-  .matchHeader('authorization', ':mypass')
-  .reply(200, [{name: 'myapp'}])
+describe('api client', () => {
+  // flow$ignore
+  Netrc.mockImplementation(() => {
+    return {machines: {'api.heroku.com': {password: 'mypass'}}}
+  })
 
-  const cmd = await Command.run([], {mock: true})
-  let response = await cmd.heroku.get('/apps')
-  expect(response).toEqual([{name: 'myapp'}])
+  let api
+  beforeEach(() => {
+    api = nock('https://api.heroku.com')
+  })
+  afterEach(() => api.done())
+
+  test('makes an HTTP request', async () => {
+    api.get('/apps')
+    .matchHeader('authorization', ':mypass')
+    .reply(200, [{name: 'myapp'}])
+
+    const cmd = await Command.run([], {mock: true})
+    const response = await cmd.heroku.get('/apps')
+    expect(response).toEqual([{name: 'myapp'}])
+  })
 })
