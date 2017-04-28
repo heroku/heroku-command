12cba4949fa623f7a3623b8c607aaab55f70e6ee
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-04-28 12:34:42 -0700

    added string flag

---
+++ src/flags/string.test.js
@@ -0,0 +1,17 @@
+// @flow
+
+import Command from '../command'
+import StringFlag from './string'
+
+class MyCommand extends Command {
+  static flags = {foo: StringFlag()}
+  foo: ?string
+  async run () {
+    this.foo = this.flags.foo
+  }
+}
+
+test('has an org', async () => {
+  const cmd = await MyCommand.mock('--foo', 'bar')
+  expect(cmd.foo).toEqual('bar')
+})
