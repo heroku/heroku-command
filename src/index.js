478ab3e0b3b3abe98fadedde0975cd3e69c4d495
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-04-28 15:56:20 -0700

    add number flag to exports (#20)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 
 export {default} from './command'
 export {default as Topic} from './topic'
-export {BooleanFlag, StringFlag, Flag} from './flags'
+export {BooleanFlag, StringFlag, NumberFlag, Flag} from './flags'
 export type {Arg} from './arg'
