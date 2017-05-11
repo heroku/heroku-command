9e608498c16c08ebc24b9ad536640c59dcba707c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-05-11 15:27:35 -0700

    fixed help newlines

--- src/help.test.js
+++ src/help.test.js
@@ -31,13 +31,77 @@ const help = new Help(buildConfig())
 describe('commandLine()', () => {
   test('has help', () => {
     expect(help.command(AppsCreate)).toEqual(`Usage: cli-engine apps:create [APP_NAME]
+
 description of apps:create
+
  -f, --foo    # foobar
  -r, --remote
  --force      # force it
+
 some
 
-multiline help\n`)
+multiline help
+`)
+  })
+
+  test('has just flags', () => {
+    expect(help.command(class extends Command {
+      static topic = 'apps'
+      static command = 'create'
+      static flags = {
+        force: boolean({description: 'force it'}),
+        app: string({char: 'a', hidden: true}),
+        foo: string({char: 'f', description: 'foobar'}),
+        remote: string({char: 'r'})
+      }
+    })).toEqual(`Usage: cli-engine apps:create
+
+ -f, --foo    # foobar
+ -r, --remote
+ --force      # force it
+`)
+  })
+
+  test('has flags + description', () => {
+    expect(help.command(class extends Command {
+      static topic = 'apps'
+      static command = 'create'
+      static description = 'description of apps:create'
+      static flags = {
+        force: boolean({description: 'force it'}),
+        app: string({char: 'a', hidden: true}),
+        foo: string({char: 'f', description: 'foobar'}),
+        remote: string({char: 'r'})
+      }
+    })).toEqual(`Usage: cli-engine apps:create
+
+description of apps:create
+
+ -f, --foo    # foobar
+ -r, --remote
+ --force      # force it
+`)
+  })
+
+  test('has description + help', () => {
+    expect(help.command(class extends Command {
+      static topic = 'apps'
+      static command = 'create'
+      static help = 'description of apps:create'
+      static flags = {
+        force: boolean({description: 'force it'}),
+        app: string({char: 'a', hidden: true}),
+        foo: string({char: 'f', description: 'foobar'}),
+        remote: string({char: 'r'})
+      }
+    })).toEqual(`Usage: cli-engine apps:create
+
+ -f, --foo    # foobar
+ -r, --remote
+ --force      # force it
+
+description of apps:create
+`)
   })
 })
 
