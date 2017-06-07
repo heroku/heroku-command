27bd3539f868201312a4d62f4b417d5dccf98fa0
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-06-06 20:50:16 -0700

    action improvements

--- src/output/action.test.js
+++ src/output/action.test.js
@@ -11,8 +11,13 @@ class Output extends Base {}
 test('shows action', () => {
   const out = new Output({mock: true})
   out.action.start('doing a foo')
+  out.stderr.log('here is a foo')
+  out.stderr.log('here is another foo')
   out.action.stop()
-  expect(out.stderr.output).toEqual('doing a foo... done\n')
+  expect(out.stderr.output).toEqual(`doing a foo...
+here is a foo
+here is another foo
+doing a foo... done\n`)
 })
 
 test('implicit done', async () => {
