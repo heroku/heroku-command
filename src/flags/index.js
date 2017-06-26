457e1ce56781019fc5311890015dca2fdd964887
RasPhilCo <capt.pn@gmail.com>
2017-06-26 13:43:15 -0700

    Have options take an Output (#41)

--- src/flags/index.js
+++ src/flags/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 import type Command from '../command'
+import type Output from '../output'
 import boolean from './boolean'
 import string from './string'
 import number from './number'
@@ -20,7 +21,7 @@ export type Flag <T> = {
   completion?: ?{
     cacheDuration?: ?number,
     cacheKey?: ?string,
-    options: () => Promise<string[]>
+    options: (Output) => Promise<string[]>
   }
 }
 
