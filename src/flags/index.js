40abd7d8d2eb50ab5da701c37de4a0befb337ec3
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-07-10 14:15:55 -0700

    prevent setting required: true on boolean flag

--- src/flags/index.js
+++ src/flags/index.js
@@ -10,6 +10,13 @@ export {default as merge} from 'lodash.merge'
 type AlphabetUppercase = | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'X' | 'Y' | 'Z'
 type AlphabetLowercase = | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'x' | 'y' | 'z'
 
+export type BooleanFlag = {
+  char?: ?(AlphabetLowercase | AlphabetUppercase),
+  description?: ?string,
+  parse?: null,
+  hidden?: ?boolean
+}
+
 export type Flag <T> = {
   char?: ?(AlphabetLowercase | AlphabetUppercase),
   description?: ?string,
