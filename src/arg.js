132d60613f69a76549d62947e65f8522e4f20a04
RasPhilCo <capt.pn@gmail.com>
2017-07-24 12:06:31 -0700

    add arg completion (#42)

--- src/arg.js
+++ src/arg.js
@@ -1,9 +1,16 @@
 // @flow
 
+import type Output from './output'
+
 export type Arg = {
   name: string,
   description?: string,
   required?: boolean,
   optional?: boolean,
-  hidden?: boolean
+  hidden?: boolean,
+  completion?: ?{
+    cacheDuration?: ?number,
+    cacheKey?: ?string,
+    options: (Output) => Promise<string[]>
+  }
 }
