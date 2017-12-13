4a0050c4dbbaebf949299c480db4c08a0fd258ec
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-13 11:42:42 -0800

    export IFlag and IArg

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Command as default, Command } from './command'
 export { Topic } from './topic'
-export { flags, Completion, FlagBuilder } from './flags'
+export { IArg } from 'cli-flags'
+export { flags, Completion, FlagBuilder, IFlag } from './flags'
